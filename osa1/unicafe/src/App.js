a0d704425e537951f5e4766c34fb270f07d4dbe9
fallmx <31561277+fallmx@users.noreply.github.com>
2022-10-23 21:13:46 +0300

    Unicafe 1.6-1.11

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,5 +1,37 @@
 import { useState } from 'react'
 
+const Header = ({text}) => <h1>{text}</h1>
+
+const Button = ({text, handleClick}) => <button onClick={handleClick}>{text}</button>
+
+const StatisticLine = ({text, value}) => <tr><td>{text}</td><td>{value}</td></tr>
+
+const Statistics = ({good, neutral, bad}) => {
+  if (!good && !neutral && !bad) {
+    return <p>No feedback given</p>
+  }
+  else {
+    let all = good + neutral + bad
+    let sum = good - bad
+
+    return (
+      <div>
+        <table>
+          <tbody>
+            <StatisticLine text="good" value={good} />
+            <StatisticLine text="neutral" value={neutral} />
+            <StatisticLine text="bad" value={bad} />
+            <StatisticLine text="all" value={all} />
+            <StatisticLine text="average" value={(sum / all).toFixed(1)} />
+            <StatisticLine text="positive" value={(good / all * 100).toFixed(1) + " %"} />
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+}
+
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
@@ -8,7 +40,12 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <Header text="give feedback" />
+      <Button text="good" handleClick={() => setGood(good + 1)} />
+      <Button text="neutral" handleClick={() => setNeutral(neutral + 1)} />
+      <Button text="bad" handleClick={() => setBad(bad + 1)} />
+      <Header text="statistics" />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
