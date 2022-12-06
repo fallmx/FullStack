802a6390a80ff08c08bf54dec3123efc3c8260ef
fallmx <31561277+fallmx@users.noreply.github.com>
2022-12-06 03:00:46 +0200

    Teht 4.8 test

---
+++ osa4/bloglist/bloglist-backend/tests/blog_api.test.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: "Tämä on title",
+    author: "Testi Mies",
+    url: "http://www.sivu.fi/"
+  },
+  {
+    title: "Toinen title",
+    author: "Jorma Jormanen",
+    url: "http://www.blogi.fi/"
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(initialBlogs)
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('correct amount of blogs is returned', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(initialBlogs.length)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
