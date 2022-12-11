bee9cbdc5e3234c67299889b85c69a821f9bcaa0
fallmx <31561277+fallmx@users.noreply.github.com>
2022-12-11 17:40:54 +0200

    Teht 4.13 and refactoring

--- osa4/bloglist/bloglist-backend/tests/blog_api.test.js
+++ osa4/bloglist/bloglist-backend/tests/blog_api.test.js
@@ -19,77 +19,104 @@ const initialBlogs = [
   }
 ]
 
-beforeEach(async () => {
-  await Blog.deleteMany({})
-  await Blog.insertMany(initialBlogs)
-})
+describe('when there are initially some blogs saved', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(initialBlogs)
+  })
+  
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+  
+  test('correct amount of blogs is returned', async () => {
+    const response = await api.get('/api/blogs')
+  
+    expect(response.body).toHaveLength(initialBlogs.length)
+  })
+  
+  test('blog has id field', async () => {
+    const response = await api.get('/api/blogs')
+  
+    expect(response.body[0].id).toBeDefined()
+  })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
+  describe('adding blogs', () => {
+    test('succeeds with valid data', async () => {
+      const newBlog = {
+        title: "This was added",
+        author: "Testi Henkilö",
+        url: "http://www.site.fi/",
+        likes: 100
+      }
+    
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+    
+      const response = await api.get('/api/blogs')
+      const titles = response.body.map(blog => blog.title)
+    
+      expect(response.body).toHaveLength(initialBlogs.length + 1)
+      expect(titles).toContain('This was added')
+    })
+    
+    test('without setting likes defaults to zero likes', async () => {
+      const noLikesBlog = {
+        title: "No likes on this",
+        author: "Testi Henkilö",
+        url: "http://www.site.fi/"
+      }
+    
+      const response = await api
+        .post('/api/blogs')
+        .send(noLikesBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+    
+      expect(response.body.likes).toBe(0)
+    })
+    
+    test('without title and url results in 400', async () => {
+      const noTitleOrUrlBlog = {
+        author: "Unknown Man",
+        likes: 3
+      }
+    
+      await api
+        .post('/api/blogs')
+        .send(noTitleOrUrlBlog)
+        .expect(400)
+    })
+  })
 
-test('correct amount of blogs is returned', async () => {
-  const response = await api.get('/api/blogs')
+  describe('deleting blogs', () => {
+    test('succeeds with status code 204 if id is valid', async () => {
+      const response = await api.get('/api/blogs')
+      const blogToDelete = response.body[0]
 
-  expect(response.body).toHaveLength(initialBlogs.length)
-})
+      await api
+        .delete(`/api/blogs/${blogToDelete.id}`)
+        .expect(204)
 
-test('blog has id field', async () => {
-  const response = await api.get('/api/blogs')
+      const newResponse = await api.get('/api/blogs')
+      const ids = newResponse.body.map(blog => blog.id)
 
-  expect(response.body[0].id).toBeDefined()
-})
+      expect(newResponse.body).toHaveLength(initialBlogs.length - 1)
+      expect(ids).not.toContain(blogToDelete.id)
+    })
 
-test('blogs can be added', async () => {
-  const newBlog = {
-    title: "This was added",
-    author: "Testi Henkilö",
-    url: "http://www.site.fi/",
-    likes: 100
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  const response = await api.get('/api/blogs')
-  const titles = response.body.map(blog => blog.title)
-
-  expect(response.body).toHaveLength(initialBlogs.length + 1)
-  expect(titles).toContain('This was added')
-})
-
-test('adding a post without setting likes defaults to zero likes', async () => {
-  const noLikesBlog = {
-    title: "No likes on this",
-    author: "Testi Henkilö",
-    url: "http://www.site.fi/"
-  }
-
-  const response = await api
-    .post('/api/blogs')
-    .send(noLikesBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  expect(response.body.likes).toBe(0)
-})
-
-test('adding a post without title and url results in 400', async () => {
-  const noTitleOrUrlBlog = {
-    author: "Unknown Man",
-    likes: 3
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(noTitleOrUrlBlog)
-    .expect(400)
+    test('fails with status code 400 if id is malformatted', async () => {
+      await api
+        .delete('/api/blogs/notanid')
+        .expect(400)
+    })
+  })
 })
 
 afterAll(() => {
