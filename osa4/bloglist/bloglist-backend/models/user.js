19cb342794194b39a50341428b15669be3df9143
fallmx <31561277+fallmx@users.noreply.github.com>
2022-12-13 01:05:45 +0200

    Teht 4.15-4.16

---
+++ osa4/bloglist/bloglist-backend/models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    minlength: [3, 'username must be at least 3 letters long'],
+    required: [true, 'username not specified'],
+    unique: true
+  },
+  name: String,
+  passwordHash: String
+})
+
+userSchema.plugin(uniqueValidator, { message: 'username must be unique' })
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+    delete returnedObject.passwordHash
+}
+})
+
+module.exports = mongoose.model('User', userSchema)
