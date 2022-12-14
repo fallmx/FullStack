a3ae1a8d0d2dad04b45b2995f10462efcdbdcf72
fallmx <31561277+fallmx@users.noreply.github.com>
2022-12-14 03:11:34 +0200

    Teht 4.22

--- osa4/bloglist/bloglist-backend/controllers/blogs.js
+++ osa4/bloglist/bloglist-backend/controllers/blogs.js
@@ -1,18 +1,15 @@
-const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-const User = require('../models/user')
+const middleware = require('../utils/middleware')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
   response.json(blogs)
 })
 
-blogsRouter.post('/', async (request, response) => {
+blogsRouter.post('/', middleware.userExtractor, async (request, response) => {
+  const user = request.user
   const blog = new Blog(request.body)
-
-  const decodedToken = jwt.verify(request.token, process.env.SECRET) 
-  const user = await User.findById(decodedToken.id)
 
   blog.user = user._id
   const savedBlog = await blog.save()
@@ -23,10 +20,8 @@ blogsRouter.post('/', async (request, response) => {
   response.status(201).json(savedBlog)
 })
 
-blogsRouter.delete('/:id', async (request, response) => {
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  const user = await User.findById(decodedToken.id)
-
+blogsRouter.delete('/:id', middleware.userExtractor, async (request, response) => {
+  const user = request.user
   const blogToDelete = await Blog.findById(request.params.id)
 
   if (blogToDelete) {
