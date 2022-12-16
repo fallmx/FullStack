ab8202b35451aaeca1572c564b2e4243aa68f7e9
fallmx <31561277+fallmx@users.noreply.github.com>
2022-12-17 00:45:02 +0200

    Teht 5.5

--- osa4/bloglist/bloglist-frontend/src/App.js
+++ osa4/bloglist/bloglist-frontend/src/App.js
@@ -1,6 +1,8 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import Blog from './components/Blog'
 import Notification from './components/Notification'
+import Togglable from './components/Togglable'
+import BlogForm from './components/BlogForm'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
@@ -14,6 +16,8 @@ const App = () => {
   const [url, setUrl] = useState('')
   const [message, setMessage] = useState(null)
   const [error, setError] = useState(false)
+
+  const blogFormRef = useRef()
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -59,18 +63,15 @@ const App = () => {
     setUser(null)
   }
 
-  const createBlog = async (event) => {
-    event.preventDefault()
+  const createBlog = async ({ title, author, url }) => {
+    blogFormRef.current.toggleVisibility()
     try {
       const newBlog = await blogService.create({
         title, author, url
       })
-      
+
       setBlogs(blogs.concat(newBlog))
 
-      setTitle('')
-      setAuthor('')
-      setUrl('')
       setError(false)
       setMessage(`a new blog ${newBlog.title} by ${newBlog.author} added`)
     } catch (exception) {
@@ -99,13 +100,11 @@ const App = () => {
       <h2>blogs</h2>
       <Notification message={message} error={error} setMessage={setMessage} />
       <p>{user.name} logged in <button onClick={logout}>logout</button></p>
-      <h2>create new</h2>
-      <form onSubmit={createBlog}>
-        <div>title: <input value={title} onChange={({target}) => setTitle(target.value)}/></div>
-        <div>author: <input value={author} onChange={({target}) => setAuthor(target.value)}/></div>
-        <div>url: <input value={url} onChange={({target}) => setUrl(target.value)}/></div>
-        <div><button type="submit">create</button></div>
-      </form>
+      
+      <Togglable buttonLabel="new blog" ref={blogFormRef}>
+        <BlogForm createBlog={createBlog} />
+      </Togglable>
+
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
