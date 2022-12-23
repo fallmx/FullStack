412d61c15e747eb5553546781771d998f125413f
fallmx <31561277+fallmx@users.noreply.github.com>
2022-12-23 22:06:12 +0200

    Teht 6.5

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -17,10 +17,13 @@ const App = () => {
     dispatch(createAnecdote(anecdote))
   }
 
+  const sortedAnecdotes = [...anecdotes]
+  sortedAnecdotes.sort((a, b) => b.votes - a.votes)
+
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
+      {sortedAnecdotes.map(anecdote =>
         <div key={anecdote.id}>
           <div>
             {anecdote.content}
