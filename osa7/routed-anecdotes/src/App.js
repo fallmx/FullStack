0de216ca220d6371f5cbd7d72826b62f3bc328fb
fallmx <31561277+fallmx@users.noreply.github.com>
2022-12-29 02:07:11 +0200

    Teht 7.2

--- osa7/routed-anecdotes/src/App.js
+++ osa7/routed-anecdotes/src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route, Link, useMatch } from 'react-router-dom'
+import Anecdote from './components/Anecdote'
 
 const Menu = () => {
   const padding = {
@@ -18,7 +19,7 @@ const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote => <li key={anecdote.id} ><Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link></li>)}
     </ul>
   </div>
 )
@@ -123,19 +124,23 @@ const App = () => {
     setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
   }
 
+  const match = useMatch('/anecdotes/:id')
+  const anecdote = match
+    ? anecdotes.find(a => a.id === Number(match.params.id))
+    : null
+
   return (
-    <Router>
-      <div>
-      <h1>Software anecdotes</h1>
-      <Menu />
-      <Routes>
-        <Route path='/' element={<AnecdoteList anecdotes={anecdotes} />} />
-        <Route path='/create' element={<CreateNew addNew={addNew} />} />
-        <Route path='/about' element={<About />} />
-      </Routes>
-      <Footer />
-    </div>
-    </Router>
+    <div>
+    <h1>Software anecdotes</h1>
+    <Menu />
+    <Routes>
+      <Route path='/' element={<AnecdoteList anecdotes={anecdotes} />} />
+      <Route path='/anecdotes/:id' element={<Anecdote anecdote={anecdote} />} />
+      <Route path='/create' element={<CreateNew addNew={addNew} />} />
+      <Route path='/about' element={<About />} />
+    </Routes>
+    <Footer />
+  </div>
   )
 }
 
